550bf3cbec293b5425b49a7485cc2725e3d86c01
Ryan-Lawton-Prog <ryan.lawton.prog@gmail.com>
2021-05-08 13:33:08 +1000

    Added add/get endpoints for blog creation

--- src/blog_endpoint.py
+++ src/blog_endpoint.py
@@ -3,12 +3,13 @@ from flask_accept import accept
 
 from helper_files.helpers import parse_json, hash_string
 from config import MongoDataBase
+import time
 
 blog_endpoint = Blueprint('blog_endpoint', __name__)
 
 DB = MongoDataBase()
-datasets = DB.get_collection("blogs")
-datasets = DB.get_collection("comments")
+blogs = DB.get_collection("blogs")
+comments = DB.get_collection("comments")
 
 delete_key = 'b339da421f0551238dcba7010211444ece30894857d608deb905babd047fb0f17a92e98287287138b4adac05e55351c3ee6aa71d1a4a99333a652f256e998dd9'
 
@@ -16,4 +17,27 @@ delete_key = 'b339da421f0551238dcba7010211444ece30894857d608deb905babd047fb0f17a
 def BLOG():
     return 'blog root, ignore'
 
-    
+@blog_endpoint.route('/blog/get_blogs', methods=['GET'])
+def get_blogs():
+    list_of_blogs = {}
+
+    for i in blogs.find():
+        ID = i['_id']
+        del i['_id']
+        list_of_blogs[str(ID)] = i
+
+    return list_of_blogs, 201
+
+@blog_endpoint.route('/blog/add_blog', methods=['POST'])
+def add_blog():
+    blog = {}
+    blog['title'] = request.json['title']
+    blog['subtitle'] = request.json['subtitle']
+    blog['author'] = request.json['author']
+    blog['date'] = time.time()
+    blog['body'] = request.json['body']
+    print(blog)
+
+    _id = str(blogs.insert_one(blog))
+    print(_id)
+    return {'_id':_id}, 201
